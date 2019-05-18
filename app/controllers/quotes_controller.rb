031aa8834d23007d2d203f190aab7c6b90000aae
Melissa <nolascm1@gmail.com>
2019-05-18 20:54:20 +0000

    Lesson 15

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -15,13 +15,17 @@ class QuotesController < ApplicationController
     redirect_to root_path
 
     end
+
+    def about
+    end
+    
     
     private
 
     def quote_params
         params.require(:quote).permit(:saying, :author)
     end
-    
+
 
 
 end
